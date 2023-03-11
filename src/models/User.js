8d54ceae5a9ebf723e308b6fbac65c920b9d5e4d
brenolg <brenolg@yahoo.com.br>
2023-03-11 14:47:06 -0300

    feat: 10 - Crie o modelo BlogPost em src/models/BlogPost.js com as propriedades e associações corretas

--- src/models/User.js
+++ src/models/User.js
@@ -22,5 +22,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
 
+User.associate = (models) => {
+User.hasMany(models.BlogPost,
+
+{ foreignKey: 'UserId', as: 'posts' }); 
+}; 
+
   return User;
 };
